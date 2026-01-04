eff73584a758470ce9f664addad1ece8c9e6d201
dhamoleom9-spec <dhamoleom9@gmail.com>
2026-01-04 18:12:28 +0530

    Reacr-Router-dom2

--- Routing-react-dom/src/Components/Corses.jsx
+++ Routing-react-dom/src/Components/Corses.jsx
@@ -3,14 +3,34 @@ import React from 'react'
 const Corses = () => {
   return (
     <div className='container co ani'>
-      <div className="sale">
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
-        <h2>sale is live..!</h2>
+      <div className="box">
+        <div className="sale">
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+        </div>
+        <div className="sale">
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+        </div>
+        <div className="sale">
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+          <h2>sale is live..!</h2>
+        </div>
       </div>
       <h1>loot lo mare ko <br /> ✊👈</h1>
       <a className='aan' href="/courses/more">see more</a>
